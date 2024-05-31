2ef9f532f566c4fc8cb36b1f9524b0586c443684
Oleksii Lavka <oleksii.lavka.dev@gmail.com>
2024-05-31 14:28:40 +0300

    updated

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -108,7 +108,9 @@ export default buildConfig({
   //     }),
   //   ],
   // }),
-  editor: lexicalEditor({ }),
+  editor: lexicalEditor({
+    features: () => ([])
+  }),
   collections: [
     Users,
     Products,
